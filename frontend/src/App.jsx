1e375d438802141d45ab876368d8ef4c3e5c49fa
kevin kalenga <kevinkalenga10@gmail.com>
2025-06-04 15:19:47 +0200

    Connexion à la base des données

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,6 +20,7 @@ function App() {
             <Route path="/profile" element={<Profile />} />
             <Route path="/about" element={<About />} />
          </Routes>
+         
       </BrowserRouter>
       
     
