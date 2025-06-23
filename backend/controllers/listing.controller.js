7ff3440f16da095c47ef5c043fc39c805965d290
kevin kalenga <kevinkalenga10@gmail.com>
2025-06-23 16:56:03 +0200

    Ajout des fonctionalités permettant de supprimer une annonce

--- backend/controllers/listing.controller.js
+++ backend/controllers/listing.controller.js
@@ -1,10 +1,29 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/errors.js";
 
 
 export const createListing = async(req, res, next) => {
     try {
         const listing = await Listing.create(req.body);
         return res.status(201).json(listing)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const deleteListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id)
+    if(!listing) {
+        return next(errorHandler(401, "L'annonce non trouvée"))
+    }
+
+    if(req.user.id !== listing.userRef) {
+        return next(errorHandler(401, 'Tu ne peux supprimer que ta propre annonce!'))
+    }
+
+    try {
+        await Listing.findByIdAndDelete(req.params.id)
+        res.status(200).json("L'annonce a été supprimée avec succèss")
     } catch (error) {
         next(error)
     }
