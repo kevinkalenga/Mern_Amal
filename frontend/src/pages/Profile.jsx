ad07ae6f69b9ec5e1ed791febb78b66b665b203a
kevin kalenga <kevinkalenga10@gmail.com>
2025-06-24 14:46:06 +0200

    Création de l'API permettant à l'utilisateur de modifier les annonces

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -265,8 +265,8 @@ export default function Profile() {
                         {listing.name}
                     </Link>
                     <div className="flex flex-col items-center">
-                        <button onClick={()=>handleListingDelete(listing._id)} className="text-red-700 uppercase">Delete</button>
-                        <button className="text-green-700 uppercase">Edit</button>
+                        <button onClick={()=>handleListingDelete(listing._id)} className="text-red-700 uppercase">Supprimer</button>
+                        <button className="text-green-700 uppercase">Editer</button>
                         
                     </div>
 
