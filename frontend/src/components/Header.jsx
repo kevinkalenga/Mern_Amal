4806675a52902df0be97eb3be16c1ad4a8e0fecd
kevin kalenga <kevinkalenga10@gmail.com>
2025-06-30 14:52:34 +0200

    Ajout de fonctionalités de filtre pour la barre de recherche

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,29 @@
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import { IoSearchOutline } from "react-icons/io5";
 import { useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
 function Header() {
+     const navigate = useNavigate()
      const {currentUser} = useSelector((state) => state.user)
-    return (
+     const [searchTerm, setSearchTerm] = useState('')
+    
+      const handleSubmit = (e) => {
+         e.preventDefault()
+         const urlParams = new URLSearchParams(window.location.search)
+         urlParams.set('searchTerm', searchTerm)
+         const searchQuery = urlParams.toString();
+         navigate(`/search?${searchQuery}`)
+      }
+
+      useEffect(() => {
+         const urlParams = new URLSearchParams(location.search)
+         const searchTermFromUrl = urlParams.get('search')
+         if(searchTermFromUrl) {
+            setSearchTerm(searchTermFromUrl)
+         }
+      }, [location.search])
+    
+     return (
        <header className="bg-slate-200 shadow-md">
           <div className="flex justify-between items-center max-w-6xl mx-auto py-2">
              <Link to='/'>
@@ -12,9 +32,10 @@ function Header() {
                     <span className='text-slate-700'>Agency</span>
                 </h1>
              </Link>
-             <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+             <form onSubmit={handleSubmit} className='bg-slate-100 p-3 rounded-lg flex items-center'>
                  <input type="text" placeholder='Recheche...' 
                     className='bg-transparent focus:outline-none w-24 sm:w-64'
+                    onChange={(e) => setSearchTerm(e.target.value)}
                  />
                  <IoSearchOutline />
              </form>
